c936064b069ff91f164539c95527f54393cbc254
Tamási Benjamin <half2me@users.noreply.github.com>
2017-09-28 13:11:10 +0200

    Update gstreamer-vaapi.rb

--- Formula/gstreamer-vaapi.rb
+++ Formula/gstreamer-vaapi.rb
@@ -3,18 +3,37 @@ class GstreamerVaapi < Formula
   homepage "https://github.com/GStreamer/gstreamer-vaapi/"
   url "https://gstreamer.freedesktop.org/src/gstreamer-vaapi/gstreamer-vaapi-1.12.3.tar.xz"
   sha256 "f4cdafd8fd9606a490917c8b67336e835df1219580d55421c70480fd0913744d"
+  
+  head do
+    url "https://github.com/GStreamer/gstreamer-vaapi.git"
+    
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
 
   depends_on "pkg-config" => :build
   depends_on "gstreamer"
-  depends_on "gst-plugins-base"
   depends_on "gst-plugins-bad"
-  depends_on "libva"
+  depends_on "linuxbrew/xorg/libva"
+  depends_on "linuxbrew/xorg/wayland" => :recommended
 
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
+    args = %W[
+      --prefix=#{prefix}
+      --disable-dependency-tracking
+      --disable-debug
+      --disable-silent-rules
+      --disable-examples
+      --with-encoders
+    ]
+    
+    if build.head?
+      # autogen is invoked in "stable" build because we patch configure.ac
+      ENV["NOCONFIGURE"] = "yes"
+      system "./autogen.sh"
+    end
+    
+    system "./configure", *args
     system "make"
     system "make", "install"
   end
