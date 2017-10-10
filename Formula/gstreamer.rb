704be3ee45a5144b54a0f5cd26de52c4a5dcbc9c
Tamási Benjamin <half2me@users.noreply.github.com>
2017-10-10 15:53:52 +0200

    Create gstreamer.rb

---
+++ Formula/gstreamer.rb
@@ -0,0 +1,54 @@
+class Gstreamer < Formula
+  desc "Development framework for multimedia applications"
+  homepage "https://gstreamer.freedesktop.org/"
+  url "https://gstreamer.freedesktop.org/src/gstreamer/gstreamer-1.12.2.tar.xz"
+  sha256 "9fde3f39a2ea984f9e07ce09250285ce91f6e3619d186889f75b5154ecf994ba"
+
+  head do
+    url "https://anongit.freedesktop.org/git/gstreamer/gstreamer.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "gobject-introspection"
+  depends_on "gettext"
+  depends_on "glib"
+  depends_on "bison"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --disable-debug
+      --disable-dependency-tracking
+      --disable-gtk-doc
+      --enable-introspection=yes
+    ]
+
+    if build.head?
+      ENV["NOCONFIGURE"] = "yes"
+      system "./autogen.sh"
+
+      # Ban trying to chown to root.
+      # https://bugzilla.gnome.org/show_bug.cgi?id=750367
+      args << "--with-ptp-helper-permissions=none"
+    end
+
+    # Look for plugins in HOMEBREW_PREFIX/lib/gstreamer-1.0 instead of
+    # HOMEBREW_PREFIX/Cellar/gstreamer/1.0/lib/gstreamer-1.0, so we'll find
+    # plugins installed by other packages without setting GST_PLUGIN_PATH in
+    # the environment.
+    inreplace "configure", 'PLUGINDIR="$full_var"',
+      "PLUGINDIR=\"#{HOMEBREW_PREFIX}/lib/gstreamer-1.0\""
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system bin/"gst-inspect-1.0"
+  end
+end
