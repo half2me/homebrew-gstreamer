b0ca0228ea6dde105f78a585c4ce8e2b0c57fec3
Tamási Benjamin <half2me@users.noreply.github.com>
2017-10-10 10:26:37 +0200

    Create gst-libav.rb

---
+++ Formula/gst-libav.rb
@@ -0,0 +1,35 @@
+class GstLibav < Formula
+  desc "GStreamer plugins for Libav (a fork of FFmpeg)"
+  homepage "https://gstreamer.freedesktop.org/"
+  url "https://gstreamer.freedesktop.org/src/gst-libav/gst-libav-1.12.2.tar.xz"
+  sha256 "5bb735b9bb218b652ae4071ea6f6be8eaae55e9d3233aec2f36b882a27542db3"
+
+  head do
+    url "https://anongit.freedesktop.org/git/gstreamer/gst-libav.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+    depends_on "gettext"
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "yasm" => :build
+  depends_on "gst-plugins-base"
+  depends_on "xz" # For LZMA
+
+  def install
+    if build.head?
+      ENV["NOCONFIGURE"] = "yes"
+      system "./autogen.sh"
+    end
+
+    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "#{Formula["gstreamer"].opt_bin}/gst-inspect-1.0", "libav"
+  end
+end
