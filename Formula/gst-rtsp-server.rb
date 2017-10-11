1549dbddfc272b5702de402c9fdf92e5133d8261
Tamási Benjamin <half2me@users.noreply.github.com>
2017-10-11 17:03:48 +0200

    Update gst-rtsp-server.rb

--- Formula/gst-rtsp-server.rb
+++ Formula/gst-rtsp-server.rb
@@ -4,6 +4,11 @@ class GstRtspServer < Formula
   url "https://gstreamer.freedesktop.org/src/gst-rtsp-server/gst-rtsp-server-1.12.3.tar.xz"
   sha256 "67255971bb16029a01de66b9f9687f20d8dbf3d3bd75feb48605d0723a7c74ec"
   
+  bottle do
+    root_url "https://lfto.me/static/bottle"
+    sha256 "866520d92e62af8afe6d525a5112d71a2fc47c5c75203aac20cbad4ac123b214" => :x86_64_linux
+  end
+  
   depends_on "half2me/gstreamer/gst-plugins-base"
 
   depends_on "libtool" => :build
