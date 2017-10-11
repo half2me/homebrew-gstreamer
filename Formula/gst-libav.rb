afb1421621b3459cca718933da3e6f398ff65ca6
Tamási Benjamin <half2me@users.noreply.github.com>
2017-10-11 17:41:45 +0200

    Update gst-libav.rb

--- Formula/gst-libav.rb
+++ Formula/gst-libav.rb
@@ -10,21 +10,32 @@ class GstLibav < Formula
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "libtool" => :build
-    depends_on "gettext"
   end
 
   depends_on "pkg-config" => :build
   depends_on "yasm" => :build
+  
   depends_on "half2me/gstreamer/gst-plugins-base"
+  depends_on "gettext"
   depends_on "xz" # For LZMA
+  
+  depends_on "orc" => :recommended
 
   def install
+    args = %W[
+      --prefix=#{prefix}
+      --disable-debug
+      --disable-dependency-tracking
+      --enable-orc=#{build.with?("orc") ? "yes" : "no"}
+      --enable-gpl=yes
+    ]
+    
     if build.head?
       ENV["NOCONFIGURE"] = "yes"
       system "./autogen.sh"
     end
 
-    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
+    system "./configure", *args
     system "make"
     system "make", "install"
   end
