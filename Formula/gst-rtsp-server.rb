e302282b4a344c8de131a05a5b45d03825cb1b64
Tamási Benjamin <half2me@users.noreply.github.com>
2017-10-11 16:39:26 +0200

    Create gst-rtsp-server.rb

---
+++ Formula/gst-rtsp-server.rb
@@ -0,0 +1,28 @@
+class GstRtspServer < Formula
+  desc "RTSP server library based on GStreamer"
+  homepage "https://gstreamer.freedesktop.org/modules/gst-rtsp-server.html"
+  url "https://gstreamer.freedesktop.org/src/gst-rtsp-server/gst-rtsp-server-1.12.3.tar.xz"
+  sha256 "67255971bb16029a01de66b9f9687f20d8dbf3d3bd75feb48605d0723a7c74ec"
+  
+  depends_on "half2me/gstreamer/gst-plugins-base"
+
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "gettext"
+  depends_on "gobject-introspection"
+
+  def install
+    system "./configure", "--prefix=#{prefix}",
+                          "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--enable-introspection=yes"
+
+    system "make", "install"
+  end
+
+  test do
+    gst = Formula["gstreamer"].opt_bin/"gst-inspect-1.0"
+    output = shell_output("#{gst} --gst-plugin-path #{lib} --plugin rtspclientsink")
+    assert_match /\s#{version.to_s}\s/, output
+  end
+end
