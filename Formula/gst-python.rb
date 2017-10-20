aaf6d936690c139e4f96cde564f58106f77e18e0
Tamási Benjamin <half2me@users.noreply.github.com>
2017-10-20 13:57:43 +0200

    Create gst-python.rb

---
+++ Formula/gst-python.rb
@@ -0,0 +1,38 @@
+class GstPython < Formula
+  desc "Python overrides for gobject-introspection-based pygst bindings"
+  homepage "https://gstreamer.freedesktop.org/modules/gst-python.html"
+  url "https://gstreamer.freedesktop.org/src/gst-python/gst-python-1.12.3.tar.xz"
+  sha256 "c3f529dec1294633132690806703b80bad5752eff482eaf81f209c2aba012ba7"
+
+  depends_on :python3
+  depends_on "pygobject3"
+  depends_on "half2me/gstreamer/gst-plugins-base"
+
+  link_overwrite "lib/python3.6/site-packages/gi/overrides"
+
+  def install
+
+    Language::Python.each_python(build) do |python, version|
+      # pygi-overrides-dir switch ensures files don't break out of sandbox.
+      system "./configure", "--disable-dependency-tracking",
+                            "--disable-silent-rules",
+                            "--prefix=#{prefix}",
+                            "--with-pygi-overrides-dir=#{lib}/python#{version}/site-packages/gi/overrides",
+                            "PYTHON=#{python}"
+      system "make", "install"
+    end
+  end
+
+  test do
+    system "#{Formula["gstreamer"].opt_bin}/gst-inspect-1.0", "python"
+    Language::Python.each_python(build) do |python, _version|
+      # Without gst-python raises "TypeError: object() takes no parameters"
+      system python, "-c", <<-EOS.undent
+        import gi
+        gi.require_version('Gst', '1.0')
+        from gi.repository import Gst
+        print (Gst.Fraction(num=3, denom=5))
+        EOS
+    end
+  end
+end
