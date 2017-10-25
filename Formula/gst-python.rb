20c78ab2b96319089e43849737bbf3863082ddb0
Tamási Benjamin <half2me@users.noreply.github.com>
2017-10-25 09:13:00 +0200

    Update gst-python.rb

--- Formula/gst-python.rb
+++ Formula/gst-python.rb
@@ -4,13 +4,28 @@ class GstPython < Formula
   url "https://gstreamer.freedesktop.org/src/gst-python/gst-python-1.12.3.tar.xz"
   sha256 "c3f529dec1294633132690806703b80bad5752eff482eaf81f209c2aba012ba7"
 
-  depends_on :python3
-  depends_on "pygobject3" => ['with-python3']
-  depends_on "half2me/gstreamer/gst-plugins-base"
+  bottle do
+    sha256 "f497d60fe94b0a1c23b446aa6a1e3ef512efd610045964c4f9d773194d7139eb" => :x86_64_linux
+  end
 
-  link_overwrite "lib/python3.6/site-packages/gi/overrides"
+  option "without-python", "Build without python 2 support"
+
+  depends_on :python3 => :optional
+  depends_on "gst-plugins-base"
+
+  depends_on "pygobject3" if build.with? "python"
+  depends_on "pygobject3" => "with-python3" if build.with? "python3"
+
+  link_overwrite "lib/python2.7/site-packages/gi/overrides"
 
   def install
+    if build.with?("python") && build.with?("python3")
+      # Upstream does not support having both Python2 and Python3 versions
+      # of the plugin installed because apparently you can load only one
+      # per process, so GStreamer does not know which to load.
+      odie "Options --with-python and --with-python3 are mutually exclusive."
+    end
+
     Language::Python.each_python(build) do |python, version|
       # pygi-overrides-dir switch ensures files don't break out of sandbox.
       system "./configure", "--disable-dependency-tracking",
@@ -26,7 +41,7 @@ class GstPython < Formula
     system "#{Formula["gstreamer"].opt_bin}/gst-inspect-1.0", "python"
     Language::Python.each_python(build) do |python, _version|
       # Without gst-python raises "TypeError: object() takes no parameters"
-      system python, "-c", <<-EOS.undent
+      system python, "-c", <<~EOS
         import gi
         gi.require_version('Gst', '1.0')
         from gi.repository import Gst
