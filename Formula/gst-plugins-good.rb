792be1763c92b27cbe9b42f3580833961a6961ef
Tamási Benjamin <half2me@users.noreply.github.com>
2017-12-11 09:22:30 +0100

    Update gst-plugins-good.rb

--- Formula/gst-plugins-good.rb
+++ Formula/gst-plugins-good.rb
@@ -1,12 +1,11 @@
 class GstPluginsGood < Formula
   desc "GStreamer plugins (well-supported, under the LGPL)"
   homepage "https://gstreamer.freedesktop.org/"
-  url "https://gstreamer.freedesktop.org/src/gst-plugins-good/gst-plugins-good-1.12.3.tar.xz"
-  sha256 "13e7f479296891fef5a686438f20ba7d534680becf2269ecc5ee24aa83b45f03"
+  url "https://gstreamer.freedesktop.org/src/gst-plugins-good/gst-plugins-good-1.12.4.tar.xz"
+  sha256 "649f49bec60892d47ee6731b92266974c723554da1c6649f21296097715eb957"
   
   bottle do
     root_url "https://lfto.me/static/bottle"
-    sha256 "fb0d3f369c9b1f8cbe328ba6cface81d053d57ab9c8a33706ba9afb23a899b10" => :x86_64_linux
   end
   
   head do
