c09548dd3801fd917284e259f08e7955fca8d0f5
Tamási Benjamin <half2me@users.noreply.github.com>
2017-12-12 12:18:21 +0100

    Update gst-rtsp-server.rb

--- Formula/gst-rtsp-server.rb
+++ Formula/gst-rtsp-server.rb
@@ -1,25 +1,25 @@
 class GstRtspServer < Formula
   desc "RTSP server library based on GStreamer"
   homepage "https://gstreamer.freedesktop.org/modules/gst-rtsp-server.html"
-  url "https://gstreamer.freedesktop.org/src/gst-rtsp-server/gst-rtsp-server-1.12.3.tar.xz"
-  sha256 "67255971bb16029a01de66b9f9687f20d8dbf3d3bd75feb48605d0723a7c74ec"
-  
+  url "https://gstreamer.freedesktop.org/src/gst-rtsp-server/gst-rtsp-server-1.12.4.tar.xz"
+  sha256 "7660112ebd59838f1054796b38109dcbe32f0a040e3a252a68a81055aeaa56a9"
+
   bottle do
-    root_url "https://lfto.me/static/bottle"
-    sha256 "866520d92e62af8afe6d525a5112d71a2fc47c5c75203aac20cbad4ac123b214" => :x86_64_linux
   end
-  
-  depends_on "half2me/gstreamer/gst-plugins-base"
 
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on "gettext"
+  depends_on "half2me/gstreamer/gst-plugins-base"
   depends_on "gobject-introspection"
 
   def install
     system "./configure", "--prefix=#{prefix}",
                           "--disable-debug",
                           "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--disable-examples",
+                          "--disable-tests",
                           "--enable-introspection=yes"
 
     system "make", "install"
@@ -27,6 +27,7 @@ class GstRtspServer < Formula
 
   test do
     gst = Formula["gstreamer"].opt_bin/"gst-inspect-1.0"
-    assert_match version.to_s, shell_output("#{gst} --plugin rtsp")
+    output = shell_output("#{gst} --gst-plugin-path #{lib} --plugin rtspclientsink")
+    assert_match /\s#{version.to_s}\s/, output
   end
 end
